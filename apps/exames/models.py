f49ac402cd230b5fcbd1684210fcf10dd984d5d5
victorpajeu <pajeuvictor@gmail.com>
2022-12-12 19:41:00 -0300

    Modelando dados

--- apps/exames/models.py
+++ apps/exames/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# models
+from apps.pacientes.models import Paciente
+
+
+class Exame(models.Model):
+    nome_profissional = models.CharField("Nome do Profissional", max_length=30)
+    data_exame = models.DateTimeField("Data do Exame")
+    peso = models.DecimalField("Peso", max_digits=7, decimal_places=2)
+    altura = models.DecimalField("Peso", max_digits=4, decimal_places=2)
+    paciente = models.ForeignKey(Paciente, verbose_name="Paciente", on_delete=models.CASCADE)
+
+
--- apps/pacientes/models.py
+++ apps/pacientes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Paciente(models.Model):
+    nome = models.CharField("Nome", max_length=30)
+    idade = models.IntegerField("Idade")
+    endereco = models.CharField("Endereço", max_length=100)
+
+
