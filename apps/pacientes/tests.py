fcf8e225bc4b0821a9e4f2c56373ca467822f3e9
victorpajeu <pajeuvictor@gmail.com>
2022-12-12 19:53:12 -0300

    Add teste

--- apps/pacientes/tests.py
+++ apps/pacientes/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+from apps.pacientes.models import Paciente
+
+
+class PacienteTest(TestCase):
+    def setUpTest(self):
+        super(PacienteTest, self).setUp()
+        self.paciente = Paciente.objects.create(nome="Joao", idade=1, endereco="Rua da conceiçao, 199")
+
+    def unicode(self):
+        self.assertEqual()
